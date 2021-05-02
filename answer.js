a4e9533775bbfef87754ee4bec9d512a80396743
ko610 <83527519+ko610@users.noreply.github.com>
2021-05-02 16:58:12 +0900

    Update answer.js

--- answer.js
+++ answer.js
@@ -1,4 +1,18 @@
+let wards = [
+    {},
+    { english: "apple", japanese: "りんご" },
+    { english: "lemon", japanese: "レモン" },
+    { english: "orange", japanese: "オレンジ" },
+    { english: "tomato", japanese: "トマト" },
+    { english: "tomato", japanese: "トマト" },
+    { english: "grape", japanese: "ぶどう" },
+    { english: "potato", japanese: "ポテト" },
+    { english: "melon", japanese: "メロン" },
+    { english: "peach", japanese: "もも" },
+    { english: "tomato", japanese: "トマト" },
+    { english: "haruya", japanese: "ハルヤ" },
 
+];
 
 //htmlタグのid取得
 let tu = document.getElementById("main_container");
